3b2f114beaf02a1d857d0806f54ac51af97212b4
Ismayel Alam <ismayelalam0@gmail.com>
2026-01-15 22:28:46 +0100

    add docker files and workflow

---
+++ apps/server/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+exec /main \
+  -db-host="${DB_HOST}" \
+  -db-port="${DB_PORT}" \
+  -db-user="${DB_USER}" \
+  -db-password="${DB_PASSWORD}" \
+  -db-dbname="${DB_DBNAME}" \
+  -db-sslmode="${DB_SSLMODE}" \
+  -db-timezone="${DB_TIMEZONE}"
