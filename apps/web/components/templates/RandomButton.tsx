6de2c50fca33c7f896e2638c1487feca09ae7566
Ismayel Alam <ismayelalam0@gmail.com>
2026-01-17 21:17:50 +0100

    redeploy

--- apps/web/components/templates/RandomButton.tsx
+++ apps/web/components/templates/RandomButton.tsx
@@ -6,15 +6,15 @@ export default function RandomButton() {
   const [randText, setRandText] = useState('Random');
 
   const getRandText = async () => {
-    const getRand = await axiosCall({
+    const getRand: string = await axiosCall({
       method: 'get',
       id: 'rand',
     });
-    setRandText(getRand);
+    setRandText(`${getRand.slice(0, 3)}-${getRand.slice(3)}`);
   };
 
   return (
-    <Button className="w-full" onClick={getRandText}>
+    <Button className="w-full text-2xl p-6" onClick={getRandText}>
       {randText}
     </Button>
   );
